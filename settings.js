cca001dc70c1cdd066738b092586b78fe7641fb4
KilerBots <davidmersey707@gmail.com>
2024-12-31 20:49:24 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,12 +1,12 @@
 const fs = require('node:fs');
 
 const config = {
-    owner: ["6285351971353", "6281910094713"],
-    name: "- nekoBot - Simple WhatsApp bot",
+    owner: ["6287701656619"],
+    name: "- NekoBot - Simple WhatsApp bot",
     sessions: "sessions",
     sticker: {
       packname: "Made by ",
-      author: "nekoBot"
+      author: "Agus"
     },
    messages: {
       wait: "> Data sedang memprosess...",
